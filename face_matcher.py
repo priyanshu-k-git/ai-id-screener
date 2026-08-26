ae1be798ea012856bca8b2441f007dc7a33fe0b1
priyanshu-k-git <emailatpkumar@gmail.com>
2026-08-26 16:36:40 +0530

    Created face_matcher.py
    Define compare_faces function for face matching.

---
+++ face_matcher.py
@@ -0,0 +1,7 @@
+# from deepface import DeepFace
+
+# Input: document_image (cv2 image matrix), selfie_image (cv2 image matrix)
+# Output: confidence_score (Float 0-100)
+# Work: Uses an AI model (like ArcFace) to find faces, extract vector embeddings, and calculate the cosine similarity to prove they are the same person.
+def compare_faces(document_image, selfie_image):
+    pass
